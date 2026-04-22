94e6e6a9ab9d75b6d23a029805853b2fc70904af
CLQueirozz <ceciliaqueiroz@usp.br>
2026-04-22 00:48:37 -0300

    feat: código que cria o pool de conexões iniciado

---
+++ backend/src/config/db.py
@@ -0,0 +1,42 @@
+import asyncpg
+import os
+from dotenv import load_dotenv
+
+
+def set_dsn():
+
+    load_dotenv()
+
+    user = os.getenv("db_username")
+    password = os.getenv("db_password")
+    port = os.getenv("db_port")
+
+    is_testing = int(os.getenv("testing"))
+
+    if is_testing:
+        name = os.getenv("db_test_name")
+        host = "localhost"
+
+    else:
+        name = os.getenv("db_name")
+        host = os.getenv("db_host")
+
+    if any(val is None for val in (user, password, port, host, name)):
+        raise ValueError("Campo faltando no .env")
+
+    return f'postgresql://{user}:{password}@{host}:{port}/{name}'
+
+
+async def create_pool():
+
+    dsn = set_dsn()
+    return await asyncpg.create_pool(
+        dsn = dsn, 
+        min_size = 5,
+        max_size = 15,
+        max_inactive_connection_lifetime = 300,
+    )
+
+
+
+
---
+++ backend/src/main.py
@@ -0,0 +1,31 @@
+from contextlib import asynccontextmanager
+import fastapi
+
+from config.db import create_pool
+from models.tables import *
+
+
+@asynccontextmanager
+async def lifespan(app: fastapi.FastAPI):
+    app.state.pool = await create_pool()
+
+    async with app.state.pool.acquire() as conn:
+        await create_table_pfp(conn)
+        await create_table_users(conn)
+        await create_table_games(conn)
+        await create_table_follows(conn)
+
+        # Outras criações de tabelas vão aqui
+
+        # Obs: Muito cuidado com a ordem de criação! Se a tabela A tem uma
+        # coluna X que depende de uma coluna Y na tabela B,chame a função
+        # create_table_B antes de create_table_A
+
+        # Fim das criações de tabelas
+
+    yield
+
+    await app.state.pool.close()
+
+
+
