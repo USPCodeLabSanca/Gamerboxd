1639a862ed5d3fa788dc8650ae6d3809bb86d6a4
ArthDias06 <arthurcastrodias@gmail.com>
2026-04-22 16:43:19 -0300

    feat: creatin of lists adn reviews tables

--- backend/src/models/tables.py
+++ backend/src/models/tables.py
@@ -71,5 +71,97 @@ async def create_table_games(conn):
         return DB_Result(success = False, message = e)
 
     else:
-        print("yupee!")
+        return DB_Result(success = True, message = "Criação da tabela funcionou!")
+
+
+async def create_table_lists(conn):
+    try:
+        await conn.execute('''
+            CREATE TABLE IF NOT EXISTS Lists (
+                list_id SERIAL PRIMARY KEY,
+                list_name VARCHAR(50) NOT NULL,
+                list_description VARCHAR(300),
+                list_creator VARCHAR(36) NOT NULL REFERENCES Users(user_id) ON DELETE CASCADE,
+                is_private BOOL NOT NULL,
+                created_at TIMESTAMPTZ DEFAULT now(),
+                last_update TIMESTAMPTZ DEFAULT now()
+            )
+        ''')
+        
+    except PostgresError as e:
+        return DB_Result(success = False, message = e)
+
+    else:
+        return DB_Result(success = True, message = "Criação da tabela funcionou!")
+
+async def create_table_list_content(conn):
+    try:
+        await conn.execute('''
+            CREATE TABLE IF NOT EXISTS list_content (
+                list INTEGER NOT NULL REFERENCES Lists(list_id) ON DELETE CASCADE,
+                game INTEGER NOT NULL REFERENCES Games(game_id) ON DELETE CASCADE,
+
+                PRIMARY KEY(list, game) 
+            )
+        ''')
+    
+    except PostgresError as e:
+        return DB_Result(success = False, message = e)
+
+    else:
+        return DB_Result(success = True, message = "Criação da tabela funcionou!")
+
+async def create_table_list_saved(conn):
+    try:
+        await conn.execute('''
+            CREATE TABLE IF NOT EXISTS list_saved (
+                user VARCHAR(36) NOT NULL REFERENCES Users(user_id) ON DELETE CASCADE,
+                list INTEGER NOT NULL REFERENCES Lists(list_id) ON DELETE CASCADE,
+
+                PRIMARY KEY(user, list)
+            )
+        ''')
+    except PostgresError as e:
+        return DB_Result(success = False, message = e)
+
+    else:
+        return DB_Result(success = True, message = "Criação da tabela funcionou!")
+
+async def create_table_reviews(conn):
+    try:
+        await conn.execute('''
+            CREATE TABLE IF NOT EXISTS Reviews (
+                review_id SERIAL PRIMARY KEY,
+                reviewer VARCHAR(36) NOT NULL REFERENCES Users(user_id) ON DELETE CASCADE,
+                game INTEGER NOT NULL REFERENCES Games(game_id) ON DELETE CASCADE,
+                rating_num FLOAT NOT NULL,
+                rating_text VARCHAR(300),
+                is_private BOOL NOT NULL,
+                time_played FLOAT NOT NULL,
+                liked BOOL NOT NULL,
+                completed BOOL NOT NULL,
+                created_at TIMESTAMPTZ DEFAULT now(),
+                last_update TIMESTAMPTZ DEFAULT now()
+            )
+        ''')
+    except PostgresError as e:
+        return DB_Result(success = False, message = e)
+
+    else:
+        return DB_Result(success = True, message = "Criação da tabela funcionou!")
+
+async def create_table_review_likes(conn):
+    try:
+        await conn.execute('''
+            CREATE TABLE IF NOT EXISTS review_likes (
+                user VARCHAR(36) NOT NULL REFERENCES Users(user_id) ON DELETE CASCADE,
+                review INTEGER NOT NULL REFERENCES Reviews(review_id) ON DELETE CASCADE,
+
+                PRIMARY KEY(user, review)
+            )
+        ''')
+    except PostgresError as e:
+        return DB_Result(success = False, message = e)
+
+    else:
         return DB_Result(success = True, message = "Criação da tabela funcionou!")
