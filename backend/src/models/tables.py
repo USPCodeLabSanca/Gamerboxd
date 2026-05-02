77206ebe8baa19fb6e6ed3fdf20139d858f0a78f
Raf4Janni <rsjannini@gmail.com>
2026-05-02 11:26:28 -0300

    criação tabelas tags, user tags, game tags, review tags e blocks

--- backend/src/models/tables.py
+++ backend/src/models/tables.py
@@ -72,3 +72,76 @@ async def create_table_games(conn):
 
     else:
         return DB_Result(success = True, message = "Criação da tabela funcionou!")
+   
+async def create_table_tags(conn):
+    try:
+        await conn.execute('''
+                CREATE TABLE IF NOT EXISTS Tags(
+                    tag_id INTEGER GENERATED ALWAYS AS IDENTITY,
+                    tag_name VARCHAR(36) UNIQUE NOT NULL 
+                )
+            ''')
+    except PostgresError as e:
+        return DB_Result(success = False, message = e)
+    else:
+        return DB_Result(success = True, message = "Criação da tabela funcionou!")
+        
+async def create_table_user_tags(conn):
+    try:
+        await conn.execute('''
+                CREATE TABLE IF NOT EXISTS UserTags(
+                    user VARCHAR(36) REFERENCES Users(user_id) ON DELETE CASCADE
+                    tag INTEGER REFERENCES Tags(tag_id) ON DELETE CASCADE
+                           
+                    PRIMARY KEY(user, tag)
+                )
+            ''')
+    except PostgresError as e:
+        return DB_Result(success = False, message = e)
+    else:
+        return DB_Result(success = True, message = "Criação da tabela funcionou!")
+    
+async def create_table_game_tags(conn):
+    try:
+        await conn.execute('''
+                CREATE TABLE IF NOT EXIST GameTags(
+                    tag INTEGER REFERENCES Tags(tag_id) ON DELETE CASCADE
+                    game INTEGER REFERENCES Games(game_id) ON DELETE CASCADE
+
+                    PRIMARY KEY (tag, game)
+                )
+            ''')
+    except PostgresError as e:
+        return DB_Result(success = False, message = e)
+    else:
+        return DB_Result(success = True, message = "Criação da tabela funcionou!")
+    
+async def create_table_review_tags(conn):
+    try:
+        await conn.execute('''
+                CREATE TABLE IF NOT EXIST ReviewTags(
+                    review VARCHAR(36) REFERENCES Reviews(review_id) ON DELETE CASCADE
+                    tag INTEGER REFERENCES Tags(tag_id) ON DELETE CASCADE
+                           
+                    PRIMARY KEY (review, tag)
+                )
+            ''')
+    except PostgresError as e:
+        return DB_Result(success = False, message = e)
+    else:
+        return DB_Result(success = True, message = "Criação da tabela funcionou!")
+    
+async def create_table_blocks(conn):
+    try:
+        await conn.execute('''
+                CREATE IF NOT EXIST Blocks(
+                    user_a REFERENCES Users(user_id) ON CASCADE DELETE
+                    user_b REFERENCES Users(user_id) ON CASCADE DELETE
+                    
+                    PRIMARY KEY (user_a, user_b)
+                )
+            ''')
+    except PostgresError as e:
+        return DB_Result(success = False, message = e)
+    else:
+        return DB_Result(success = True, message = "Criação da tabela funcionou!")
