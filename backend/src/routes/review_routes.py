668f7382081c845f6ca5a6ffdb533d819fe3aa23
CLQueirozz <ceciliaqueiroz@usp.br>
2026-05-07 14:35:20 -0300

    fix: arquivo das rotas de review adicionado

---
+++ backend/src/routes/review_routes.py
@@ -0,0 +1,11 @@
+from fastapi import Depends, status, Request
+from fastapi.responses import JSONResponse
+from fastapi_utils.cbv import cbv
+from fastapi_utils.inferring_router import InferringRouter
+
+from models.schemas import *
+from services.security_services import *
+from services.db_services import *
+from utils.dependencies import get_conn, require_login
+
+review_router = InferringRouter(prefix="/review", tags=["review"])
