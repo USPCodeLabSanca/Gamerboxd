b0d0ecc6a3c5217d29f69fa8e056e28a3a7d3c59
joaopneves1570 <bolotaneves@gmail.com>
2026-06-03 00:02:36 -0300

    fix: resolvendo conflitos de merge

--- frontend/src/components/MemberCard.jsx
+++ frontend/src/components/MemberCard.jsx
@@ -1,5 +1,8 @@
-import { GamepadDirectional, List, Heart } from "lucide-react";
 import { formatNumber } from "../utils/formatters";
+
+import likedIcon from "../assets/icons/liked.svg";
+import jogouIcon from "../assets/icons/jogou.svg";
+import listaIcon from "../assets/icons/lista.svg";
 
 export default function MemberCard({ player }) {
   return (
@@ -40,20 +43,17 @@ export default function MemberCard({ player }) {
 
       <div className="flex flex-wrap gap-4 md:gap-0 text-zinc-400 text-sm">
         <div className="w-auto md:w-24 flex items-center gap-2">
-          <GamepadDirectional size={14} className="text-roxo fill-roxo" />
+          <img src={jogouIcon} className="w-5"></img>
           <span>{formatNumber(player.gamesPlayed)}</span>
         </div>
 
         <div className="w-auto md:w-24 flex items-center gap-2">
-          <List size={14} className="text-white fill-white" />
+          <img src={listaIcon} className="w-5"></img>
           <span>{formatNumber(player.reviews)}</span>
         </div>
 
         <div className="w-auto md:w-24 flex items-center gap-2">
-          <Heart
-            size={14}
-            className="text-red-500 fill-red-500"
-          />
+          <img src={likedIcon} className="w-5"></img>
           <span>{formatNumber(player.likes)}</span>
         </div>
       </div>
